28e2cf98e88c135a389b96dbaa688e26c11bf3f0
ElvisMieles3 <elvismieles@gmail.com>
2020-07-19 09:28:59 -0500

    se elimina la carpeta ejemplos

--- cypress/integration/examples/search.js
+++
@@ -1,8 +0,0 @@
-describe('Search test cases', function () {
-        it('Search with results', function () {
-                cy.visit('http://automationpractice.com/index.php');
-                cy.get('#search_query_top').type('Dress');
-                cy.get('#searchbox > .btn').click();
-                cy.get('.lighter').contains('"dress"');
-        })
-})
