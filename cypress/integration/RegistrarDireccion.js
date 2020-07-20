d10d2c856e6058cf54f5d03e648122b100521ae0
ElvisMieles3 <elvismieles@gmail.com>
2020-07-20 17:13:20 -0500

    Se agrega el comando para el loguin

--- cypress/integration/RegistrarDireccion.js
+++ cypress/integration/RegistrarDireccion.js
@@ -1,23 +1,23 @@
-describe('Agregar dirección a un usuario que se encuentre registrado en el portal',function(){
-    beforeEach(function(){
+describe('Agregar dirección a un usuario que se encuentre registrado en el portal', function () {
+    beforeEach(function () {
+        cy.fixture('login.json').as('datosLoguin');
         cy.visit('/');
-       })
-   
-   
-    it('login', function(){
-        cy.get('[title="Log in to your customer account"]').click();
-      
-        cy.fixture('login.json')
-       .then(({user,password}) =>{
-       cy.log(user);
-       cy.get('#email').type(user);
-       cy.get('#passwd').type(password,{log: false});
-       cy.get('#SubmitLogin > span').click();
-    });
-       cy.get('[title="View my customer account"]')
-       .type('Elvis Miguel Mieles Guarin');
+    })
 
-       
-      
-   })
+
+    it('login', function () {
+        cy.get('[title="Log in to your customer account"]').click();
+        cy.server();
+        cy.route('POST', 'api/login').as('loginRequesT');
+        cy.get('@datosLoguin')
+            .then(({ user, password }) => {
+                cy.log(user);
+                cy.login({ user, password });
+               // cy.wait('@loginRequesT');
+                cy.url().should('include', 'my-account');
+                cy.get('#SubmitLogin > span').click();
+                cy.get('[title="View my customer account"]')
+                    .type('Elvis Miguel Mieles Guarin');
+            });
+    });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', ({ user, password }) => {
+    cy.get('#email').type(user);
+    cy.get('#passwd').type(password, { log: false });
+});
